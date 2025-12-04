6dee7e0924a8b3bec278df48adbb04f15b32c47c
Gideon <gidewadejob@gmail.com>
2025-12-04 19:34:07 +0100

    Removed code from template and added empty map page

---
+++ frontend/client/lib/features/map/presentation/pages/map_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "I AM TEXT LOOK AT ME",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
