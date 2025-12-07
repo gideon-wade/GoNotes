bd99b36414e5ca4753801f3c74ee775e2b53f886
Gideon Wade <88282777+gideon-wade@users.noreply.github.com>
2025-12-07 19:56:46 +0100

    CLIENT Added new workflows for frontend (#3)
    Features
    
    - Separate workflows for Android, Web, and iOS builds.
    - Test and analysis workflow for client.

--- frontend/client/test/widget_test.dart
+++ frontend/client/test/widget_test.dart
@@ -1,2 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/app.dart';
+
 void main() {
+  testWidgets('Check map page loads', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('GoNotes Map'), findsOneWidget);
+  });
 }
