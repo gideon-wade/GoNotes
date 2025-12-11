b57e1c816ca5d035623276200c2cf24427becb14
Gideon <gidewadejob@gmail.com>
2025-12-11 21:02:27 +0100

    started working on themese

---
+++ frontend/client/lib/core/theme/color_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorTheme {
+  Color get primaryColor;
+  Color get primaryVariantColor;
+  Color get secondaryColor;
+  Color get secondaryVariantColor;
+  Color get backgroundColor;
+  Color get surfaceColor;
+  Color get errorColor;
+  Color get onPrimaryColor;
+  Color get onSecondaryColor;
+  Color get onBackgroundColor;
+  Color get onSurfaceColor;
+  Color get onErrorColor;
+  Color get textColor;
+}
---
+++ frontend/client/lib/core/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:client/core/theme/color_theme.dart';
+
+class Theme {
+  ColorTheme colorTheme;
+
+  Theme({required this.colorTheme});
+}
