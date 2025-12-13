15f3522ee261eb939a29fe71e26fbe3c3300c0fe
Gideon <gidewadejob@gmail.com>
2025-12-13 20:07:41 +0100

    fixed test

--- frontend/client/test/widget_test.dart
+++ frontend/client/test/widget_test.dart
@@ -1,9 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:client/app.dart';
 
 void main() {
   testWidgets('Check map page loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const MaterialApp(home: App()));
+
+    final loginButton = find.widgetWithText(ElevatedButton, 'Login');
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
     expect(find.text('GoNotes Map'), findsOneWidget);
+  });
+
+  testWidgets('Can switch between Login and Sign Up tabs', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const App());
+
+    final signUpTab = find.ancestor(
+      of: find.text('Sign Up'),
+      matching: find.byType(Tab),
+    );
+
+    await tester.tap(signUpTab);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Confirm Password'), findsOneWidget);
   });
 }
