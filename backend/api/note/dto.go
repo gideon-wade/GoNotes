597f80398283387357d66795e1c7810714ac1d93
Daniel Sommer <daniel8770@gmail.com>
2025-12-30 14:58:35 +0100

    Implemented endpoints for getting note by id and getting all notes

--- backend/api/note/dto.go
+++ backend/api/note/dto.go
@@ -3,19 +3,31 @@ package note
 import "time"
 
 type NoteResponseDTO struct {
-	ID      string    `json:"id" binding:"required"`
-	UserID  string    `json:"userID" binding:"required"`
-	Title   string    `json:"title" binding:"required"`
-	Content string    `json:"content" binding:"required"`
-	Date    time.Time `json:"date" binding:"required"`
+	ID        string    `json:"id" binding:"required"`
+	UserID    string    `json:"userID" binding:"required"`
+	Title     string    `json:"title" binding:"required"`
+	Content   string    `json:"content" binding:"required"`
+	Date      time.Time `json:"date" binding:"required"`
+	Latitude  float32   `json:"latitude" binding:"required"`
+	Longitude float32   `json:"longitude" binding:"required"`
+}
+
+type NewNoteRequestDTO struct {
+	UserID    string  `json:"userID" binding:"required"`
+	Title     string  `json:"title" binding:"required"`
+	Content   string  `json:"content" binding:"required"`
 	Latitude  float32 `json:"latitude" binding:"required"`
 	Longitude float32 `json:"longitude" binding:"required"`
 }
 
-type NewNoteRequestDTO struct {
-	UserID  string `json:"userID" binding:"required"`
-	Title   string `json:"title" binding:"required"`
-	Content string `json:"content" binding:"required"`
-	Latitude  float32 `json:"latitude" binding:"required"`
-	Longitude float32 `json:"longitude" binding:"required"`
+func (note *Note) ToNoteResponseDTO() *NoteResponseDTO {
+	return &NoteResponseDTO{
+		ID:        note.ID,
+		UserID:    note.UserID,
+		Title:     note.Title,
+		Content:   note.Content,
+		Date:      note.Date,
+		Longitude: note.Longitude,
+		Latitude:  note.Latitude,
+	}
 }
